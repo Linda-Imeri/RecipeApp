b9e894941899acc8aeedc37dca70c9b21ab0f06c
Linda-Imeri <lindaimmeri@gmail.com>
2022-11-10 03:58:33 +0100

    Update RecipeView, display recipe details

---
+++ RecipeApp/Views/RecipeDetailView.swift
@@ -0,0 +1,52 @@
+//
+//  RecipeDetailView.swift
+//  RecipeApp
+//
+//  Created by Linda Imeri on 10.11.22.
+//
+
+import SwiftUI
+
+struct RecipeDetailView: View {
+    var recipe: Recipe
+    var body: some View {
+        ScrollView{
+            VStack(alignment: .leading){
+                //MARK: Recipe Image
+                Image(recipe.image).resizable()
+                    .scaledToFit()
+                
+                //MARK: Ingredients
+                VStack(alignment: .leading){
+                    Text("Ingredients")
+                        .font(.headline)
+                        .padding([.bottom,.top],5)
+                    
+                    ForEach(recipe.ingredients,id: \.self){ ingredient in
+                        Text("- " + ingredient)
+                        
+                    }
+                }
+                Divider()
+                //MARK: Directions
+                VStack(alignment: .leading){
+                    Text("Directions")
+                        .font(.headline)
+                        .padding(.bottom,5)
+                    
+                    ForEach(0..<recipe.directions.count,id: \.self) {
+                        index in
+                        Text(String(index+1) + ".  " + recipe.directions[index]).padding(.bottom, 5)
+                    }
+                }
+            }.padding()
+        }
+    }
+}
+
+struct RecipeDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        let viewModel = RecipeViewModel()
+        RecipeDetailView(recipe: viewModel.recipeArray[0])
+    }
+}
