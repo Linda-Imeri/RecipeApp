d9ce45ddaf6f8007ce8a2dfb96eaa075aaf7826c
Linda-Imeri <lindaimmeri@gmail.com>
2022-11-11 23:41:09 +0100

    Create Recipe Card, Use Geometry Reader for Rectangle frame, Show list of recipes that should be featured

--- RecipeApp/Views/RecipeFeaturedView.swift
+++ RecipeApp/Views/RecipeFeaturedView.swift
@@ -10,12 +10,31 @@ import SwiftUI
 struct RecipeFeaturedView: View {
     @EnvironmentObject var viewModel : RecipeViewModel
     var body: some View {
-        Text("hello")
+        GeometryReader{ geo in
+            TabView{
+                ForEach(0..<viewModel.recipeArray.count){ index in
+                    
+                    //Show only those that should be feautured
+                    if viewModel.recipeArray[index].featured {
+                        
+                        //Recipe Card
+                        Rectangle()
+                            .frame(width: geo.size.width - 40, height: geo.size.height - 100, alignment: .center)
+                            .cornerRadius(15)
+                            .shadow(radius: 10)
+                    }
+                }
+            }
+            .tabViewStyle(PageTabViewStyle(indexDisplayMode: .automatic))
+            .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .always))
+            .shadow(radius: 10)
+        }
     }
 }
 
 struct RecipeFeaturedView_Previews: PreviewProvider {
     static var previews: some View {
         RecipeFeaturedView()
+            .environmentObject(RecipeViewModel())
     }
 }
