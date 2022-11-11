a33a603984a7db3fc1446e1a07b5f5010f716351
Linda-Imeri <lindaimmeri@gmail.com>
2022-11-11 18:12:05 +0100

    Create RecipeFeaturedView

---
+++ RecipeApp/Views/RecipeFeaturedView.swift
@@ -0,0 +1,20 @@
+//
+//  RecipeFeaturedView.swift
+//  RecipeApp
+//
+//  Created by Linda Imeri on 11.11.22.
+//
+
+import SwiftUI
+
+struct RecipeFeaturedView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RecipeFeaturedView_Previews: PreviewProvider {
+    static var previews: some View {
+        RecipeFeaturedView()
+    }
+}
